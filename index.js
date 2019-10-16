8d0189950bd5354cdf04f8be98221fb23c9759a5
prasadPegasus <pegasus@Pegasuss-MacBook-Air.local>
2019-10-16 11:47:06 +0530

    categories and vod

--- index.js
+++ index.js
@@ -1,16 +1,17 @@
 const http = require("http");
 const fs = require("fs");
+const path=require('path');
 http
   .createServer((req, res) => {
     res.setHeader("Content-Type", "application/json");    
     if (req.url == "/categories") {
-      fs.readFile("./categories.json", (err, data) => {
+      fs.readFile(path.join(__dirname,"./categories.json"), (err, data) => {
           if(data)
             res.end(data.toString());
       });
     }
     if (req.url == "/vod") {
-        fs.readFile("./vod.json", (err, data) => {
+        fs.readFile(path.join(__dirname,"./vod.json"), (err, data) => {
             if(data)
                 res.end(data.toString());
         });
