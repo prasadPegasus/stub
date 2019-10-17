7007971cf0171b3db975fb68f862e046d815642e
prasadPegasus <pegasus@Pegasuss-MacBook-Air.local>
2019-10-17 16:17:13 +0530

    categories based on home

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const http = require("http");
 const fs = require("fs");
-const path=require('path');
+const path = require("path");
 http
   .createServer((req, res) => {
-    res.setHeader("Content-Type", "application/json");    
+    res.setHeader("Content-Type", "application/json");
+    console.log(req.url);
+
     if (req.url == "/categories") {
-      fs.readFile(path.join(__dirname,"./categories.json"), (err, data) => {
-          if(data)
-            res.end(data.toString());
+      fs.readFile(path.join(__dirname, "./categories.json"), (err, data) => {
+        if (data) res.end(data.toString());
+      });
+    }
+    if (req.url == "/categories?home=true") {
+      fs.readFile(path.join(__dirname, "./categories.json"), (err, data) => {
+        if (data) res.end(data.toString());
       });
     }
     if (req.url == "/vod") {
-        fs.readFile(path.join(__dirname,"./vod.json"), (err, data) => {
-            if(data)
-                res.end(data.toString());
-        });
-      }
+      fs.readFile(path.join(__dirname, "./vod.json"), (err, data) => {
+        if (data) res.end(data.toString());
+      });
+    }
   })
   .listen(8088);
